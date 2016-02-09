02e85fbb7aab37365015dae62c74846e36f8adb1
nachomanriqueperez <joseim@zeta40.aulas.gsyc.es>
2016-02-09 14:48:06 +0100

    Hecho el ejercicio sumador aleatorio

--- 5.5.4/5.5.4.js
+++ 5.5.4/5.5.4.js
@@ -1,6 +1,14 @@
+/*
+ * Esto es un borrador de JavaScript.
+ *
+ * Escriba algo de JavaScript, después haga clic derecho o elija del menú Acción:
+ * 1. Ejecutar para evaluar el texto seleccionado (Ctrl+R),
+ * 2. Inspeccionar para mostrar el inspector de objetos con el resultado (Ctrl+I), o,
+ * 3. Mostrar para insertar el resultado en un comentario después de la selección. (Ctrl+L)
+ */
 function changer (id, newValue) {
   var element = document.getElementById(id);
   element.innerHTML = newValue;
 }
 
-changer("body", "Hola");
+changer("rt-page-surround", "Hola");
